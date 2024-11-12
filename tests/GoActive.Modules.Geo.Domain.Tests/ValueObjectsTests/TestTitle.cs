de7d6eeb6a397a119073a7d1a0c1dbf1e6f9eb43
Pavel-art <pv.mazanov@yandex.ru>
2024-11-12 23:07:34 +0300

    feat: create tests for title

---
+++ tests/GoActive.Modules.Geo.Domain.Tests/ValueObjectsTests/TestTitle.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+using GoActive.Modules.Geo.Domain.ValueObjects;
+
+namespace GoActive.Modules.Geo.Domain.Tests.ValueObjectsTests;
+
+public class TestTitle
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData(" ")]
+    [InlineData("")]
+    public void CreateTitle_WrongValue(string title)
+    {
+        FluentActions.Invoking(() => _ = Title.FromValue(title))
+            .Should()
+            .ThrowExactly<ArgumentException>()
+            .WithMessage("Title cannot be null or whitespace.");
+    }
+
+    [Fact]
+    public void CreateTitle_ValidValue()
+    {
+        var value = "A valid title";
+        var title = Title.FromValue(value);
+        title.Value.Should().Be(value);
+    }
+}
