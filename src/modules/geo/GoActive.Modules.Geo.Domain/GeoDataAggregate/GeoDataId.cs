c3d1a05b1c479bc7ed6e098a287aa811e191eed6
Kirill4a <kirill4a@gmail.com>
2024-11-19 06:24:32 +0100

    migrated to TheoryData in title tests

--- src/modules/geo/GoActive.Modules.Geo.Domain/GeoDataAggregate/GeoDataId.cs
+++ src/modules/geo/GoActive.Modules.Geo.Domain/GeoDataAggregate/GeoDataId.cs
@@ -4,9 +4,9 @@ namespace GoActive.Modules.Geo.Domain.GeoDataAggregate;
 public readonly record struct GeoDataId(Guid Value)
 {
     // TODO: allow initialize from this factory method only
-    public static GeoDataId FromValue(Guid guid)
+    public static GeoDataId FromValue(Guid value)
     {
-        return guid == Guid.Empty ? throw new ArgumentException("Value should be non-empty Guid", nameof(guid))
-            : new(guid);
+        return value == Guid.Empty ? throw new ArgumentException("Value should be non-empty Guid", nameof(value))
+            : new(value);
     }
 };
